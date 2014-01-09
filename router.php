182f5deb3e6f0b4070e69530e237adf160bebdc6
Uri Goren <uri@goren4u.com>
2014-01-09 12:41:55 +0200

    router now gets the api as $_GET parameter

--- router.php
+++ router.php
@@ -1,28 +1,21 @@
 <?php
-	$query=str_replace('/','',$_SERVER['REQUEST_URI']);
-	//uncomment this line if your api is case insensitive
-	//$query=strtolower($query);
-	$query=str_replace('.php','',$query);
-	//if a php file is found in the root folder, use it
-	if (file_exists("$query.php"))
-	{
-		if ($query!='router')
-			include ("$query.php");
-		die();
-	}
-	//if a php file exists in the api folder, call its function
-	if (file_exists("api/$query.php"))
-	{
-		include ("api/$query.php");
-		if (function_exists($query))
-		{
-			$data = file_get_contents("php://input");
-			$objData = json_decode($data);
-			$arr=call_user_func($query,$objData);
-			echo json_encode($arr);
-			die();
-		}
-	}
-	//handle 404 error
-	echo 'file not found';
+        $query=$_GET['api'];
+        //if a php file exists in the api folder, call its function
+        if (file_exists("api/$query.php"))
+        {
+                include ("api/$query.php");
+                if (function_exists($query))
+                {
+                        $data = file_get_contents("php://input");
+                        $objData = json_decode($data);
+						if (json_last_error() == JSON_ERROR_NONE)
+							$arr=call_user_func($query,$objData);
+						else
+							$arr=call_user_func($query,$data);
+                        echo json_encode($arr);
+                        die();
+                }
+        }
+        //handle 404 error
+        echo 'file not found';
 ?>
